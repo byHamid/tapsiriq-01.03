bf936484ebb76dafc35c680cf4fc9bb933bc8071
Hamid <hamisgen24@gmail.com>
2025-03-07 20:06:52 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-let num1 = 2;
-let num2 = 4;
-const num3 = 6;
-const num4 = 8;
-const num5 = 10;
-const num6 = 12;
-const cem = num1 + num2;
-const ferq = num4 - num3;
-const hasil = num1 * num5;
-const bolme = num6 / num3;
-console.log(num1,num2,num3,num4,num5,num6,cem,ferq,hasil,bolme);
-alert(num1,num2,num3,num4,num5,num6,cem,ferq,hasil,bolme)
+let name = prompt("Adinizi yazin");
+let age = prompt("Yaşinizi yazin");
+let hobbies = "";
+if (confirm("Do you want to write ur hobies?")) {
+    hobbies = prompt("Write your hobies with comma");
+}
+alert(`Ad: ${name}\nYaş ${age}\nHobbilər: ${hobbies}`);
